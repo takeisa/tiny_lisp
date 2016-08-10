f38ec0625c7d6956142ffe582f99b067fcd4040d
Satoshi Takei <s.takei.dev@gmail.com>
2016-08-10 18:10:33 +0900

    Initial commit

---
+++ make_configure.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+cat > Makefile.am <<\
+    "--------------"
+AM_YFLAGS=-d
+AM_CFLAGS=-g -Wall -O2
+bin_PROGRAMS=tlisp
+tlisp_SOURCES=main.c
+tlisp_CFLAGS=$(AM_CFLAGS) -O0
+#calc_LDADD=@LEXLIB@
+--------------
+
+autoscan
+
+sed -e 's/FULL-PACKAGE-NAME/tlisp/' \
+    -e 's/VERSION/1/' \
+    -e 's|BUG-REPORT-ADDRESS|/dev/null|' \
+    -e '10i\
+AM_INIT_AUTOMAKE\
+# AM_PROG_LEX\
+# AC_PROG_YACC\
+CFLAGS="-g -O0"\
+AC_PROG_CC_C99\
+' \
+    < configure.scan > configure.ac
+
+touch NEWS README AUTHORS ChangeLog
+autoreconf -iv
+./configure
+make distcheck
