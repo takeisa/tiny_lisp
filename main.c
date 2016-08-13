6d3cd1afe5a60fcad5a93e7c153903c62de90202
Satoshi Takei <satoshi@atom>
2016-08-13 11:13:47 +0900

    Chapter4 interactive prompt

--- main.c
+++ main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
+#define MAX_BUF_SIZE 2048
+
+static char input[MAX_BUF_SIZE];
+
 int main(int argc, char *argv[]) {
-  puts("hello, world!");
+  puts("TLisp Version 0.01");
+  puts("Press Ctrl+c to Exit\n");
+
+  while (1) {
+    fputs("tlisp> ", stdout);
+    fgets(input, MAX_BUF_SIZE, stdin);
+    printf("No you're a %s", input);
+  }
+
   return 0;
 }
